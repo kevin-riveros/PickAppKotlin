9ff68f349b41f56c80ce566f0a7a0c49ecdd5120
kevinriverosguia <riverosk885@gmail.com>
2017-12-13 13:41:35 -0500

    commit

--- app/src/main/java/pe/com/kevinrsd/myapplicationkotlin/MainActivity.kt
+++ app/src/main/java/pe/com/kevinrsd/myapplicationkotlin/MainActivity.kt
@@ -1,12 +1,18 @@
 package pe.com.kevinrsd.myapplicationkotlin
 
 import android.support.v7.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    val text="seduction"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txt=findViewById<TextView>(R.id.txtname)
+        txt.setText(text)
     }
 }
